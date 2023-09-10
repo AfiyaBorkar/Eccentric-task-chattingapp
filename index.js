384560b74b9db5778a9a92851119e160815482fd
Afiya <afiyaborkar34@gmail.com>
2023-09-10 13:08:32 +0530

    fix:production

--- index.js
+++ index.js
@@ -4,26 +4,14 @@ const mongoose = require("mongoose");
 const authRoutes = require("./routes/auth");
 const messageRoutes = require("./routes/messages");
 const app = express();
-const socket = require("socket.io");
+const http = require("http").Server(app); // Use the http module for the server
+const io = require("socket.io")(http); // Initialize Socket.io with the http server
 require("dotenv").config();
-const Group = require("./models/groupModel")
-const path = require("path")
-
-
-
+const Group = require("./models/groupModel");
+const path = require("path");
 
 app.use(cors());
 app.use(express.json());
-
-
-
-// app.use(express.static(path.resolve(__dirname, '../client/build')));
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
-// });
-
-
-
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -32,70 +20,54 @@ mongoose
   })
   .then(async () => {
     console.log("DB Connection Successful");
-
-    // Check if there are any documents in the Group collection
     const count = await Group.countDocuments();
 
     if (count === 0) {
-      // Create a default collection if no documents exist
       const allUsersGroup = await Group.create({});
       await allUsersGroup.save();
     }
   })
   .catch((err) => {
-    console.log(err.message);
+    console.error("DB Connection Error:", err);
   });
-
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-const server = app.listen(process.env.PORT, () =>
-  console.log(`Server started on ${process.env.PORT}`)
-);
-const io = socket(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    credentials: true,
-  },
-});
+// Serve your static frontend files if needed
+// app.use(express.static(path.resolve(__dirname, '../client/build')));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
+// });
 
-global.onlineUsers = new Map();
+// Socket.io connection handling
+const onlineUsers = new Map();
+
 io.on("connection", (socket) => {
-  global.chatSocket = socket;
   socket.on("add-user", (userId) => {
     onlineUsers.set(userId, socket.id);
   });
 
-  // socket.on("send-msg", (data) => {
-  //   const sendUserSocket = onlineUsers.get(data.to);
-  //   if (sendUserSocket) {
-  //     socket.to(sendUserSocket).emit("msg-recieve", data.msg);
-  //   }
-  // });
   socket.on("send-msg", (data) => {
-    console.log("🚀 ~ file: index.js:68 ~ socket.on ~ data:", data)
+    console.log("Received message:", data);
 
-   
-
-    console.log(onlineUsers)
-    if(data.type=="group")
-    {
-        // Broadcast the message to all online users
-        onlineUsers.forEach((userSocket) => {
-          socket.to(userSocket).emit("msg-recieve", data);
-        });
-    }
-    else
-    {
+    if (data.type === "group") {
+      // Broadcast the message to all online users
+      onlineUsers.forEach((userSocket) => {
+        socket.to(userSocket).emit("msg-recieve", data);
+      });
+    } else {
       const sendUserSocket = onlineUsers.get(data.to);
       if (sendUserSocket) {
         socket.to(sendUserSocket).emit("msg-recieve", data);
       }
     }
-
   });
+});
 
-  
+const PORT = process.env.PORT || 3000; // Use the provided PORT or default to 3000
 
+// Start the server
+http.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
