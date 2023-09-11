63aa6437c88a0f092d5d7be9595df422a29db6ae
Afiya <afiyaborkar34@gmail.com>
2023-09-11 13:59:10 +0530

    fix

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/auth");
 const messageRoutes = require("./routes/messages");
@@ -7,6 +8,7 @@ const app = express();
 const socket = require("socket.io");
 require("dotenv").config();
 const Group = require("./models/groupModel");
+app.use(express.static(path.resolve(__dirname, './client/build')));
 
 app.use(cors()); // Use the CORS middleware
 app.use(express.json());
@@ -30,8 +32,13 @@ mongoose
     console.log(err.message);
   });
 
-app.use("/api/auth", authRoutes);
+  app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+  app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './client/build/index.html'));
+  });
+  
+
 
 const server = app.listen(process.env.PORT || 5000, () =>
   console.log(`Server started on ${process.env.PORT}`)
