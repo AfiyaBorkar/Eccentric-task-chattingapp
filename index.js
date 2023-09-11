370c1e4049b5cf995b52c859f2bd81f93f94b238
Afiya <afiyaborkar34@gmail.com>
2023-09-11 12:25:21 +0530

    Fixed for production

--- index.js
+++ index.js
@@ -4,14 +4,25 @@ const mongoose = require("mongoose");
 const authRoutes = require("./routes/auth");
 const messageRoutes = require("./routes/messages");
 const app = express();
-const http = require("http").Server(app); // Use the http module for the server
-const io = require("socket.io")(http); // Initialize Socket.io with the http server
+const socket = require("socket.io");
 require("dotenv").config();
-const Group = require("./models/groupModel");
-const path = require("path");
+const Group = require("./models/groupModel")
+
+
+
 
 app.use(cors());
 app.use(express.json());
+
+
+
+// app.use(express.static(path.resolve(__dirname, '../client/build')));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
+// });
+
+
+
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -20,54 +31,70 @@ mongoose
   })
   .then(async () => {
     console.log("DB Connection Successful");
+
+    // Check if there are any documents in the Group collection
     const count = await Group.countDocuments();
 
     if (count === 0) {
+      // Create a default collection if no documents exist
       const allUsersGroup = await Group.create({});
       await allUsersGroup.save();
     }
   })
   .catch((err) => {
-    console.error("DB Connection Error:", err);
+    console.log(err.message);
   });
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-// Serve your static frontend files if needed
-// app.use(express.static(path.resolve(__dirname, '../client/build')));
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
-// });
+const server = app.listen(process.env.PORT, () =>
+  console.log(`Server started on ${process.env.PORT}`)
+);
+const io = socket(server, {
+  cors: {
+    origin: "*",
+    credentials: true,
+  },
+});
 
-// Socket.io connection handling
-const onlineUsers = new Map();
-
+global.onlineUsers = new Map();
 io.on("connection", (socket) => {
+  global.chatSocket = socket;
   socket.on("add-user", (userId) => {
     onlineUsers.set(userId, socket.id);
   });
 
+  // socket.on("send-msg", (data) => {
+  //   const sendUserSocket = onlineUsers.get(data.to);
+  //   if (sendUserSocket) {
+  //     socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+  //   }
+  // });
   socket.on("send-msg", (data) => {
-    console.log("Received message:", data);
+    console.log("🚀 ~ file: index.js:68 ~ socket.on ~ data:", data)
 
-    if (data.type === "group") {
-      // Broadcast the message to all online users
-      onlineUsers.forEach((userSocket) => {
-        socket.to(userSocket).emit("msg-recieve", data);
-      });
-    } else {
+   
+
+    console.log(onlineUsers)
+    if(data.type=="group")
+    {
+        // Broadcast the message to all online users
+        onlineUsers.forEach((userSocket) => {
+          socket.to(userSocket).emit("msg-recieve", data);
+        });
+    }
+    else
+    {
       const sendUserSocket = onlineUsers.get(data.to);
       if (sendUserSocket) {
         socket.to(sendUserSocket).emit("msg-recieve", data);
       }
     }
+
   });
-});
 
-const PORT = process.env.PORT || 3000; // Use the provided PORT or default to 3000
+  
 
-// Start the server
-http.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
 });
